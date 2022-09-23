02628ce12e52a6a7f683e7d317a42b046ddf1b7a
Amfiteatr42 <108868537+Amfiteatr42@users.noreply.github.com>
2022-09-23 18:49:48 +0300

    Update 03-feedback.js

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -5,12 +5,11 @@ const form = document.querySelector('.feedback-form');
 const inputEmail = document.querySelector('[name="email"]');
 const inputMessage = document.querySelector('[name="message"]');
 const inputValues = {};
-// console.log(JSON.parse(localStorage.feedback).email);
-// console.log(localStorage.getItem('feedback'));
+const STORAGE_KEY = 'feedback-form-state';
 
-if (localStorage.feedback) {
-  inputEmail.value = JSON.parse(localStorage.feedback).email;
-  inputMessage.value = JSON.parse(localStorage.feedback).message;
+if (localStorage.getItem(STORAGE_KEY)) {
+  inputEmail.value = JSON.parse(localStorage.getItem(STORAGE_KEY)).email;
+  inputMessage.value = JSON.parse(localStorage.getItem(STORAGE_KEY)).message;
 }
 
 form.addEventListener('input', throttle(onFormInput, 500));
@@ -20,7 +19,7 @@ function onFormInput(e) {
   formData.forEach((value, x) => {
     inputValues[x] = value;
 
-    save('feedback', inputValues);
+    save(STORAGE_KEY, inputValues);
   });
 }
 
@@ -28,8 +27,7 @@ form.addEventListener('submit', onFormSubmit);
 
 function onFormSubmit(e) {
   e.preventDefault();
-  console.log(JSON.parse(localStorage.feedback));
-  localStorage.removeItem('feedback');
-  inputEmail.value = '';
-  inputMessage.value = '';
+  // console.log(JSON.parse(localStorage.STORAGE_KEY));
+  localStorage.removeItem(STORAGE_KEY);
+  e.currentTarget.reset();
 }
